225845c93dd2ca6a23b9bdb2877fa2fa1712d0b3
Jacob Qvist <jqjqvist@gmail.com>
2017-09-07 14:36:27 +0200

    Start over

--- src/hotciv/standard/GameImpl.java
+++ src/hotciv/standard/GameImpl.java
@@ -31,152 +31,60 @@ import hotciv.framework.*;
 
 public class GameImpl implements Game {
 
-    //Players
-    private Player redPlayer;
-    private Player bluePlayer;
-    private Player currentPlayer;
-    private int age;
 
-    private CityImpl redCity;
-    private CityImpl blueCity;
-    private MapCreator map;
-
-
-    public GameImpl() {
-        //We start the game out with two players. red and blue with the corresponding enum types.
-        //We also set a currentPlayer object as red, because red always start.
-        this.map = new MapCreator();
-
-        //Units
-        //puts all the starting units and constants into the map
-        map.putGameUnit(new Position(2, 0), new UnitImpl(GameConstants.ARCHER, Player.RED));
-        map.putGameUnit(new Position(3, 2), new UnitImpl(GameConstants.LEGION, Player.BLUE));
-        map.putGameUnit(new Position(4, 3), new UnitImpl(GameConstants.SETTLER, Player.BLUE));
-        //Constants
-        map.putGameConstant(new Position(1, 0), GameConstants.OCEANS);
-        map.putGameConstant(new Position(2, 2), GameConstants.MOUNTAINS);
-        map.putGameConstant(new Position(0, 1), GameConstants.HILLS);
-
-        this.age = -4000;
-
-        this.redPlayer = Player.RED;
-        this.bluePlayer = Player.BLUE;
-        this.currentPlayer = this.redPlayer;
-
-        this.redCity = new CityImpl(new Position(1, 1), redPlayer);
-        this.blueCity = new CityImpl(new Position(4, 1), bluePlayer);
-    }
-
-    public MapCreator getMap() {
-        return map;
-    }
-
+    @Override
     public Tile getTileAt(Position p) {
         return null;
     }
 
+    @Override
     public Unit getUnitAt(Position p) {
-        return map.getGameUnit(p);
-    }
-
-    private String getSurfaceType(Position p){ return map.getGameConstant(p);}
-
-    public City getCityAt(Position p) {
-        if (p.getRow() == redCity.getRow() && p.getColumn() == redCity.getColumn()) {
-            return redCity;
-        }
-        if (p.getRow() == blueCity.getRow() && p.getColumn() == blueCity.getColumn()) {
-            return blueCity;
-        } else {
-            return null;
-        }
-    }
-
-    public Player getPlayerInTurn() {
-        return this.currentPlayer;
-    }
-    //Winner is always Player.RED at -3000BC
-    public Player getWinner() {
-        if (age == -3000) {
-            return Player.RED;
-        }
         return null;
     }
 
+    @Override
+    public City getCityAt(Position p) {
+        return null;
+    }
+
+    @Override
+    public Player getPlayerInTurn() {
+        return null;
+    }
+
+    @Override
+    public Player getWinner() {
+        return null;
+    }
+
+    @Override
     public int getAge() {
-        return age;
+        return 0;
     }
 
+    @Override
     public boolean moveUnit(Position from, Position to) {
-        //Moves unit from position from to position to.
-        if ((map.getGameUnit(from) != null) && (map.getGameUnit(from).getOwner() == currentPlayer) && (map.getGameUnit(from).getMoveCount() > 0)) {
-            if(isValidMove(from, to)){
-                map.putGameUnit(to, map.getGameUnit(from));
-                map.getGameUnit(to).moveUnit();
-                map.deleteGameUnit(from);
-                return true;
-            }
-        }
         return false;
     }
 
-    private boolean isValidMove(Position from, Position to){
-        if(isInsideMap(to) && isNotAMountain(to)) {
-            if (from.getRow() <= to.getRow() + 1 && from.getRow() >= to.getRow() - 1) {
-                if (from.getColumn() <= to.getColumn() + 1 && from.getColumn() >= to.getColumn() - 1)
-                    return true;
-            }
-        }
-        return false;
-    }
-    private boolean isNotAMountain(Position to){
-        return !((getSurfaceType(new Position(to.getRow(), to.getColumn())).equals("mountain")) || (getSurfaceType(new Position(to.getRow(), to.getColumn())).equals("ocean")));
-    }
-
-    //Checks whether the player is about to move the object outside the map or not.
-    private boolean isInsideMap(Position to){
-        return(to.getColumn()<16 && to.getRow()<16 && to.getColumn()>=0 && to.getRow()>=0);
-    }
-
+    @Override
     public void endOfTurn() {
-        //Changes the player in turn and after ended round (All players turn) production is increased by 6, movecount is
-        //reset and age is incremented by 100
-        if (currentPlayer == Player.RED) {
-            currentPlayer = this.bluePlayer;
-        } else {
-            //We increase production of all cities by 6 after last players turn.
-            redCity.increaseProduction();
-            blueCity.increaseProduction();
-            map.resetAllMovesCount();
-            currentPlayer = this.redPlayer;
-            age+=100;
-        }
 
     }
 
+    @Override
     public void changeWorkForceFocusInCityAt(Position p, String balance) {
+
     }
 
+    @Override
     public void changeProductionInCityAt(Position p, String unitType) {
 
     }
 
+    @Override
     public void performUnitActionAt(Position p) {
-    }
 
-    public int getPopulationAtCity(CityImpl city) {
-        return city.getSize();
-    }
-
-
-    public void attackUnit(Position position, Position position1) {
-        if(getUnitAt(position1).getOwner()!=currentPlayer) {
-            moveUnit(position, position1);
-        }
-    }
-    public void spawnRedArcher(){
-        //Just spawning the new one at 15, 15 atm
-        map.putGameUnit(new Position(15,15),new UnitImpl(GameConstants.ARCHER,Player.RED));
     }
 }
 

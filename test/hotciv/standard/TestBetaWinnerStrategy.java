c72a210be380d22f9a75a66df335a042971fcb40
Jacob Qvist <jqjqvist@gmail.com>
2017-09-18 16:18:14 +0200

    iteration 60: betaWinnerStrategy implemented. Refactored blueCityPos to remove dublication.

--- test/hotciv/standard/TestBetaWinnerStrategy.java
+++ test/hotciv/standard/TestBetaWinnerStrategy.java
@@ -1,6 +1,7 @@
 package hotciv.standard;
 
 import hotciv.framework.Player;
+import hotciv.framework.Position;
 import hotciv.standard.StrategyClasses.BetaWinnerStrategy;
 import hotciv.standard.StrategyInterfaces.WinnerStrategy;
 import org.junit.Before;
@@ -13,16 +14,25 @@ public class TestBetaWinnerStrategy {
 
     private WinnerStrategy winnerStrategy;
     private GameImpl game;
+    private Position blueCityPos;
 
     @Before
     public void setup(){
+        this.blueCityPos = new Position(4,1);
         winnerStrategy = new BetaWinnerStrategy();
         game = new GameImpl(winnerStrategy);
     }
 
     @Test
     public void shouldMakePlayerWinnerIfHeConquresAllCities(){
-
+        game.moveUnit(new Position(2,0), new Position(3,1));
+        //advances the game 5 rounds, to spawn a settler at Blue City.
+        for (int i = 0; i < 10; i++){
+            game.endOfTurn();
+        }
+        assertThat(game.getCityAt(blueCityPos).getOwner(), is(Player.BLUE));
+        game.moveUnit(new Position(3,1), blueCityPos);
+        assertThat(game.getCityAt(blueCityPos).getOwner(), is(Player.RED));
         assertThat(game.getWinner(), is(Player.RED));
     }
 

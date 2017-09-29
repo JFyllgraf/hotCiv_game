7861da52480ab6eb5a0ddbeff72faec802789ede
Jacob Qvist <jqjqvist@gmail.com>
2017-09-29 22:22:24 +0200

    Test fail in attackingStrategy fixed for now. The class was not correctly set up

--- test/hotciv/standard/TestAlphaAttackingStrategy.java
+++ test/hotciv/standard/TestAlphaAttackingStrategy.java
@@ -1,7 +1,19 @@
 package hotciv.standard;
-
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
 /**
  * Created by csdev on 9/29/17.
  */
 public class TestAlphaAttackingStrategy {
+
+    @Before
+    public void setup(){
+
+    }
+
+    @Test
+    public void shouldSomething(){
+
+    }
 }
--- test/hotciv/standard/TestEpsilonAttackingStrategy.java
+++ test/hotciv/standard/TestEpsilonAttackingStrategy.java
@@ -1,7 +1,18 @@
 package hotciv.standard;
-
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
 /**
  * Created by csdev on 9/29/17.
  */
 public class TestEpsilonAttackingStrategy {
+    @Before
+    public void setup(){
+
+    }
+
+    @Test
+    public void shouldSomething(){
+
+    }
 }
